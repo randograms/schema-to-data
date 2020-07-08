49f2f39c9ce36b41a12d33a4d1b8c804ac5fa6e4
Rachid Lamouri <rachidlamouri@gmail.com>
2020-07-07 21:06:52 -0400

    Add more tests cases to generateData for malformed schemas

--- tests/unit/generateData.test.js
+++ tests/unit/generateData.test.js
@@ -1,6 +1,26 @@
 const { lib } = require('../..');
 
 describe('generateData', function () {
+  context('with a schema with an undefined type', function () {
+    it('throws an error', function () {
+      const testFn = () => {
+        lib.generateData({ type: undefined });
+      };
+
+      expect(testFn).to.throw('Expected schema to have a known type but got "undefined"');
+    });
+  });
+
+  context('with a schema with an non-string type', function () {
+    it('throws an error', function () {
+      const testFn = () => {
+        lib.generateData({ type: 87 });
+      };
+
+      expect(testFn).to.throw('Expected schema to have a known type but got "87"');
+    });
+  });
+
   context('with a schema with an unknown type', function () {
     it('throws an error', function () {
       const testFn = () => {
