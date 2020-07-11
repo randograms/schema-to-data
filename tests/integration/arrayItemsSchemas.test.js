1c2bd156f3132683812bd3ddc4ceb2641eb70148
Rachid Lamouri <rachidlamouri@gmail.com>
2020-07-11 13:05:25 -0400

    Separate array item schema integration tests from array schema tests

---
+++ tests/integration/arrayItemsSchemas.test.js
@@ -0,0 +1,48 @@
+const { mapBasicSchemas } = require('./helpers/commonSchemas');
+
+describe('array items schemas', function () {
+  // TODO: test that the generated array does not have to have every item
+  testSchema({
+    scenario: 'when items is a tuple',
+    schema: {
+      type: 'array',
+      items: [{ type: 'string' }, { type: 'number' }, { type: 'boolean' }],
+    },
+  });
+
+  testSchema({
+    scenario: 'with typeless items',
+    schema: { type: 'array' },
+    runCount: 30,
+    itSometimesValidatesAgainst: mapBasicSchemas(({ descriptor, basicSchema }) => ({
+      itSometimesReturns: `an array with ${descriptor}`,
+      contains: basicSchema,
+    })),
+  });
+
+  testSchema({
+    scenario: 'with single typed items',
+    schema: {
+      type: 'array',
+      items: { type: 'string' },
+    },
+  });
+
+  testSchema({
+    scenario: 'with multi-typed items',
+    schema: {
+      type: 'array',
+      items: { type: ['string', 'boolean'] },
+    },
+    itSometimesValidatesAgainst: [
+      {
+        itSometimesReturns: 'an array with an item of the first type',
+        contains: { type: 'string' },
+      },
+      {
+        itSometimesReturns: 'an array with an item of the second type',
+        contains: { type: 'boolean' },
+      },
+    ],
+  });
+});
--- tests/integration/arraySchemas.test.js
+++ tests/integration/arraySchemas.test.js
@@ -1,66 +1,20 @@
-const { mapBasicSchemas } = require('./helpers/commonSchemas');
-
 describe('array schemas', function () {
   testSchema({
     scenario: 'with just type',
     schema: { type: 'array' },
-    runCount: 30,
     itSometimesValidatesAgainst: [
       {
         itSometimesReturns: 'an empty array',
         maxItems: 0,
       },
-      ...mapBasicSchemas(({ descriptor, basicSchema }) => ({
-        itSometimesReturns: `an array with ${descriptor}`,
-        contains: basicSchema,
-      })),
-    ],
-  });
-
-  // TODO: test that the generated array does not have to have every item
-  testSchema({
-    scenario: 'when items is a tuple',
-    schema: {
-      type: 'array',
-      items: [{ type: 'string' }, { type: 'number' }, { type: 'boolean' }],
-    },
-  });
-
-  testSchema({
-    scenario: 'with typeless items',
-    schema: {
-      type: 'array',
-      items: {},
-    },
-    runCount: 30,
-    itSometimesValidatesAgainst: mapBasicSchemas(({ descriptor, basicSchema }) => ({
-      itSometimesReturns: `an array with ${descriptor}`,
-      contains: basicSchema,
-    })),
-  });
-
-  testSchema({
-    scenario: 'with single typed items',
-    schema: {
-      type: 'array',
-      items: { type: 'string' },
-    },
-  });
-
-  testSchema({
-    scenario: 'with multi-typed items',
-    schema: {
-      type: 'array',
-      items: { type: ['string', 'boolean'] },
-    },
-    itSometimesValidatesAgainst: [
       {
-        itSometimesReturns: 'an array with an item of the first type',
-        contains: { type: 'string' },
+        itSometimesReturns: 'an array with one item',
+        minItems: 1,
+        maxItems: 1,
       },
       {
-        itSometimesReturns: 'an array with an item of the second type',
-        contains: { type: 'boolean' },
+        itSometimesReturns: 'an array with multiple items',
+        minItems: 3,
       },
     ],
   });
