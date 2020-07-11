9f5da6c05481bb2fa3ee4a9b520a50f473bc4a18
Rachid Lamouri <rachidlamouri@gmail.com>
2020-07-11 13:21:32 -0400

    Separate object properties schema integration tests from object schema tests

--- tests/integration/objectSchemas.test.js
+++ tests/integration/objectSchemas.test.js
@@ -1,5 +1,3 @@
-const { mapBasicSchemas } = require('./helpers/commonSchemas');
-
 describe('object schemas', function () {
   // TODO: test that this always returns an empty object (for now)
   testSchema({
@@ -73,45 +71,5 @@ describe('object schemas', function () {
         additionalProperties: false,
       },
     ],
-  });
-
-  testSchema({
-    scenario: 'with multi-typed properties',
-    schema: {
-      type: 'object',
-      properties: {
-        required: { type: ['number', 'boolean'] },
-      },
-      required: ['required'],
-    },
-    itSometimesValidatesAgainst: [
-      {
-        itSometimesReturns: 'an object with a property that has the first type',
-        properties: { required: { type: 'number' } },
-        required: ['required'],
-      },
-      {
-        itSometimesReturns: 'an object with a property that has the second type',
-        properties: { required: { type: 'boolean' } },
-        required: ['required'],
-      },
-    ],
-  });
-
-  testSchema({
-    scenario: 'with typless properties',
-    schema: {
-      type: 'object',
-      properties: {
-        required: {},
-      },
-      required: ['required'],
-    },
-    runCount: 30,
-    itSometimesValidatesAgainst: mapBasicSchemas(({ descriptor, basicSchema }) => ({
-      itSometimesReturns: `an object with ${descriptor} property`,
-      properties: { required: basicSchema },
-      required: ['required'],
-    })),
   });
 });
