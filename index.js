de93d05bb2c3ddb32f4a28bccf9a3a040e42efab
Rachid Lamouri <rachidlamouri@gmail.com>
2020-07-12 16:03:27 -0400

    Support default string length range

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const _ = require('lodash');
 const faker = require('faker');
 
 // TODO: allow these to be configured
+const minStringLength = 0;
+const maxStringLength = 20;
 const minInteger = -100000;
 const maxInteger = 100000;
 const minNumber = minInteger;
@@ -101,8 +103,28 @@ const generateData = (singleTypedSchema) => {
 };
 
 const generateString = () => {
-  const randomWord = faker.lorem.word();
-  return randomWord;
+  const stringLength = _.random(minStringLength, maxStringLength);
+
+  let generatedLength = 0;
+  const randomWords = [];
+
+  while (generatedLength < stringLength) {
+    let randomWord = faker.lorem.word();
+    generatedLength += randomWord.length;
+
+    if (generatedLength > stringLength) {
+      const numCharsToRemove = generatedLength - stringLength;
+      const numCharsToKeep = randomWord.length - numCharsToRemove;
+
+      randomWord = randomWord.substr(0, numCharsToKeep);
+      generatedLength -= numCharsToRemove;
+    }
+
+    randomWords.push(randomWord);
+  }
+
+  const randomString = randomWords.join('');
+  return randomString;
 };
 
 const generateNumber = () => {
--- tests/integration/stringSchemas.test.js
+++ tests/integration/stringSchemas.test.js
@@ -1,10 +1,27 @@
 describe('string schemas', function () {
-  // TODO: test that default string length can vary
   testSchema({
     scenario: 'by default',
     schema: {
       itAlwaysReturns: 'a string',
       type: 'string',
     },
+    runCount: 30,
+    itValidatesAgainst: [
+      {
+        itSometimesReturns: 'an empty string',
+        maxLength: 0,
+      },
+      {
+        itSometimesReturns: 'a short string',
+        minLength: 1,
+        maxLength: 10,
+      },
+      {
+        itSometimesReturns: 'a long string',
+        minLength: 11,
+        maxLength: 20,
+      },
+    ],
+  });
   });
 });
