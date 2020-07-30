71ae7fe3e98950f14a952046a1d94548fdc3adcb
Rachid Lamouri <rachidlamouri@gmail.com>
2020-07-30 18:23:09 -0400

    Support custom string defaults

--- tests/unit/getConformedStringKeywords.test.js
+++ tests/unit/getConformedStringKeywords.test.js
@@ -1,4 +1,15 @@
-const { defaultMocker } = require('../../lib/mocker');
+const _ = require('lodash');
+const { defaultMocker, Mocker } = require('../../lib/mocker');
+
+const setupCustomMocker = (options) => {
+  before(function () {
+    this.mocker = new Mocker(options);
+
+    _.keys(options).forEach((option) => {
+      expect(this.mocker.DEFAULTS[option]).to.not.equal(defaultMocker.DEFAULTS[option]);
+    });
+  });
+};
 
 const generateValidTestSchema = ({ ...keywords } = {}) => ({
   ...keywords,
@@ -7,28 +18,65 @@ const generateValidTestSchema = ({ ...keywords } = {}) => ({
 
 describe('getConformedStringKeywords', function () {
   context('by default', function () {
-    it('returns relevant keys', function () {
+    it('returns relevant keys with default values', function () {
       const singleTypedSchema = generateValidTestSchema();
       expect(defaultMocker.getConformedStringKeywords(singleTypedSchema)).to.eql({
         minLength: 0,
-        maxLength: 20,
+        maxLength: 500,
       });
     });
   });
 
-  context('when minLength exceeds the default maxLength', function () {
-    before(function () {
-      const singleTypedSchema = generateValidTestSchema({
-        minLength: 1000,
-      });
+  context('with a custom default "minStringLength"', function () {
+    setupCustomMocker({ minStringLength: 30 });
 
-      this.result = defaultMocker.conformSchemaToType(singleTypedSchema);
+    it('returns relevant keys with custom default values', function () {
+      const singleTypedSchema = generateValidTestSchema();
+      expect(this.mocker.getConformedStringKeywords(singleTypedSchema)).to.eql({
+        minLength: 30,
+        maxLength: 530,
+      });
+    });
+  });
+
+  context('with a custom default "stringLengthRange"', function () {
+    setupCustomMocker({
+      stringLengthRange: 80,
     });
 
-    it('adjusts the maxLength', function () {
-      expect(this.result).to.be.like({
-        minLength: 1000,
-        maxLength: 1020,
+    it('returns relevant keys with custom default values', function () {
+      const singleTypedSchema = generateValidTestSchema();
+      expect(this.mocker.getConformedStringKeywords(singleTypedSchema)).to.eql({
+        minLength: 0,
+        maxLength: 80,
+      });
+    });
+  });
+
+  context('with "minLength" and a custom default "stringLengthRange"', function () {
+    setupCustomMocker({ stringLengthRange: 20 });
+
+    it('returns the "minLength" and an adjusted "maxLength"', function () {
+      const singleTypedSchema = generateValidTestSchema({
+        minLength: 70,
+      });
+      expect(this.mocker.getConformedStringKeywords(singleTypedSchema)).to.eql({
+        minLength: 70,
+        maxLength: 90,
+      });
+    });
+  });
+
+  context('with "maxLength"', function () {
+    setupCustomMocker({ minStringLength: 1 });
+
+    it('returns the default "minStringLength" and the "maxLength', function () {
+      const singleTypedSchema = generateValidTestSchema({
+        maxLength: 60,
+      });
+      expect(this.mocker.getConformedStringKeywords(singleTypedSchema)).to.eql({
+        minLength: 1,
+        maxLength: 60,
       });
     });
   });
--- tests/unit/mocker.test.js
+++ tests/unit/mocker.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-new */
+
 const { Mocker, defaultMocker } = require('../../lib/mocker');
 
 describe('mocker', function () {
@@ -5,10 +7,63 @@ describe('mocker', function () {
     context('with unsupported options', function () {
       it('throws an error', function () {
         const testFn = () => {
-          new Mocker({ badOptionA: true, badOptionB: true }); // eslint-disable-line no-new
+          new Mocker({ badOptionA: true, badOptionB: true });
         };
 
         expect(testFn).to.throw('Unsupported option(s) "badOptionA,badOptionB"');
+      });
+    });
+
+    describe('string default errors', function () {
+      context('when "minStringLength" is less than zero', function () {
+        it('throws an error', function () {
+          const testFn = () => {
+            new Mocker({ minStringLength: -1 });
+          };
+
+          expect(testFn).to.throw('"minStringLength" must be a non-negative integer');
+        });
+      });
+
+      context('when "minStringLength" is not an integer', function () {
+        it('throws an error', function () {
+          const testFn = () => {
+            new Mocker({ minStringLength: 2.7 });
+          };
+
+          expect(testFn).to.throw('"minStringLength" must be a non-negative integer');
+        });
+      });
+
+      context('when "stringLengthRange" is less than zero', function () {
+        it('throws an error', function () {
+          const testFn = () => {
+            new Mocker({ stringLengthRange: -1 });
+          };
+
+          expect(testFn).to.throw('"stringLengthRange" must be a non-negative integer');
+        });
+      });
+
+      context('when "stringLengthRange" is not an integer', function () {
+        it('throws an error', function () {
+          const testFn = () => {
+            new Mocker({ stringLengthRange: 3.5 });
+          };
+
+          expect(testFn).to.throw('"stringLengthRange" must be a non-negative integer');
+        });
+      });
+    });
+
+    context('with multiple errors', function () {
+      it('throws all errors', function () {
+        const testFn = () => {
+          new Mocker({ minStringLength: -1, badOptionA: true });
+        };
+
+        // eslint-disable-next-line max-len
+        expect(testFn).to.throw('Error(s) setting up defaults: "minStringLength" must be a non-negative integer, Unsupported option(s) "badOptionA"');
       });
     });
   });
