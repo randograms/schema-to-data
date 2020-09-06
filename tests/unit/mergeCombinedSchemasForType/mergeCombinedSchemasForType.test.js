101dd50834ae2fadd048fb3e581646138c395257
Rachid Lamouri <rachidlamouri@gmail.com>
2020-09-06 18:27:38 -0400

    Update merge rules for nested combined schemas

--- tests/unit/mergeCombinedSchemasForType/mergeCombinedSchemasForType.test.js
+++ tests/unit/mergeCombinedSchemasForType/mergeCombinedSchemasForType.test.js
@@ -6,7 +6,7 @@ const { defaultMocker } = require('../../../lib/mocker');
  * The merge rules for all of the keywords are thoroughly tested in another file
  */
 describe('mergeCombinedSchemasForType/mergeCombinedSchemasForType', function () {
-  context('when the schema has an allOf', function () {
+  context('when the schema has an "allOf"', function () {
     before(function () {
       this.singleTypedSchema = {
         type: 'object',
@@ -15,14 +15,24 @@ describe('mergeCombinedSchemasForType/mergeCombinedSchemasForType', function () 
         required: ['a'],
         allOf: [
           {
+            type: 'object',
             minProperties: 2,
             required: ['b'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
           },
           {
+            type: 'object',
             maxProperties: 9,
             required: ['c'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
           },
         ],
+        anyOf: null,
+        oneOf: null,
       };
       this.result = testUnit(defaultMocker, 'mergeCombinedSchemasForType', this.singleTypedSchema);
     });
@@ -41,73 +51,31 @@ describe('mergeCombinedSchemasForType/mergeCombinedSchemasForType', function () 
     });
   });
 
-  // TODO: consolidate this into a set of tests that verifies combinations of nested combined schemas work
-  context('when the schema has an allOf with an allOf', function () {
+  context('when the schema has an "anyOf"', function () {
     before(function () {
       this.singleTypedSchema = {
         type: 'object',
-        minProperties: 1,
-        maxProperties: 10,
-        required: ['a'],
-        allOf: [
-          {
-            minProperties: 2,
-            required: ['b'],
-          },
-          {
-            maxProperties: 9,
-            required: ['c'],
-            allOf: [
-              {
-                minProperties: 3,
-                required: ['d'],
-              },
-              {
-                maxProperties: 8,
-                required: ['e'],
-              },
-            ],
-          },
-        ],
-      };
-      this.result = testUnit(defaultMocker, 'mergeCombinedSchemasForType', this.singleTypedSchema);
-    });
-
-    it('does not modify the original schema', function () {
-      expect(this.result).to.not.equal(this.singleTypedSchema);
-    });
-
-    it('merges the combined schemas into the root schema', function () {
-      expect(this.result).to.eql({
-        type: 'object',
-        minProperties: 3,
-        maxProperties: 8,
-        required: ['a', 'b', 'c', 'd', 'e'],
-      });
-    });
-  });
-
-  context('when the schema has an anyOf', function () {
-    before(function () {
-      this.singleTypedSchema = {
-        type: 'object',
+        allOf: null,
         anyOf: [
           {
-            // type intentionally left out
+            type: 'object',
             required: ['a'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
           },
           {
             type: 'object',
             required: ['b'],
-          },
-          {
-            type: 'string',
-            maxLength: 4,
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
           },
         ],
+        oneOf: null,
       };
 
-      this.results = _.times(10, () => testUnit(defaultMocker, 'mergeCombinedSchemasForType', this.singleTypedSchema));
+      this.results = _.times(30, () => testUnit(defaultMocker, 'mergeCombinedSchemasForType', this.singleTypedSchema));
     });
 
     it('always merges at least one relevant subschema', function () {
@@ -136,91 +104,31 @@ describe('mergeCombinedSchemasForType/mergeCombinedSchemasForType', function () 
     });
   });
 
-  // TODO: consolidate this into a set of tests that verifies combinations of nested combined schemas work
-  context('when the schema has an anyOf with an anyOf', function () {
-    before(function () {
-      this.singleTypedSchema = {
-        type: 'object',
-        anyOf: [
-          {
-            type: 'object',
-            required: ['a'],
-          },
-          {
-            type: 'object',
-            required: ['b'],
-            anyOf: [
-              {
-                type: 'object',
-                required: ['c'],
-              },
-              {
-                type: 'object',
-                required: ['d'],
-              },
-            ],
-          },
-        ],
-      };
-
-      this.results = _.times(50, () => testUnit(defaultMocker, 'mergeCombinedSchemasForType', this.singleTypedSchema));
-    });
-
-    const expectedVariations = [
-      {
-        type: 'object',
-        required: ['a'],
-      },
-      {
-        type: 'object',
-        required: ['b', 'c'],
-      },
-      {
-        type: 'object',
-        required: ['b', 'd'],
-      },
-      {
-        type: 'object',
-        required: ['b', 'c', 'd'],
-      },
-      {
-        type: 'object',
-        required: ['a', 'b', 'c', 'd'],
-      },
-    ];
-
-    it('always merges at least one relevant subschema', function () {
-      expect(this.results).to.all.satisfy((schema) => _.isArray(schema.required) && schema.required.length > 0);
-    });
-
-    expectedVariations.forEach((expectedResult, index) => {
-      it(`sometimes merges subschemas: ${expectedResult.required.join()}`, function () {
-        expect(this.results, `case ${index} failed`).to.include.something.that.eqls(expectedResult);
-      });
-    });
-  });
-
   context('when the schema has a "oneOf"', function () {
     before(function () {
       this.singleTypedSchema = {
         type: 'object',
+        allOf: null,
+        anyOf: null,
         oneOf: [
           {
             type: 'object',
             required: ['a'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
           },
           {
-            // type intentionally left out
+            type: 'object',
             required: ['b'],
-          },
-          {
-            type: 'string',
-            maxLength: 4,
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
           },
         ],
       };
 
-      this.results = _.times(10, () => testUnit(defaultMocker, 'mergeCombinedSchemasForType', this.singleTypedSchema));
+      this.results = _.times(20, () => testUnit(defaultMocker, 'mergeCombinedSchemasForType', this.singleTypedSchema));
     });
 
     it('always merges one compatible subschema', function () {
@@ -240,6 +148,138 @@ describe('mergeCombinedSchemasForType/mergeCombinedSchemasForType', function () 
       expect(this.results).to.include.something.that.eqls({
         type: 'object',
         required: ['b'],
+      });
+    });
+  });
+
+  context('when the schema has an "allOf", "anyOf" and "oneOf"', function () {
+    before(function () {
+      const singleTypedSchema = {
+        type: 'object',
+        required: ['a'],
+        allOf: [{
+          type: 'object',
+          required: ['b'],
+          allOf: null,
+          anyOf: null,
+          oneOf: null,
+        }],
+        anyOf: [{
+          type: 'object',
+          required: ['c'],
+          allOf: null,
+          anyOf: null,
+          oneOf: null,
+        }],
+        oneOf: [{
+          type: 'object',
+          required: ['d'],
+          allOf: null,
+          anyOf: null,
+          oneOf: null,
+        }],
+      };
+
+      this.result = testUnit(defaultMocker, 'mergeCombinedSchemasForType', singleTypedSchema);
+    });
+
+    it('merges all nested combined schemas', function () {
+      expect(this.result).to.eql({
+        type: 'object',
+        required: ['a', 'b', 'c', 'd'],
+      });
+    });
+  });
+
+  context('when the schema has multiple nested combined schemas', function () {
+    before(function () {
+      const singleTypedSchema = {
+        type: 'object',
+        required: ['a'],
+        allOf: [{
+          type: 'object',
+          required: ['b'],
+          allOf: [{
+            type: 'object',
+            required: ['c'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+          anyOf: [{
+            type: 'object',
+            required: ['d'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+          oneOf: [{
+            type: 'object',
+            required: ['e'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+        }],
+        anyOf: [{
+          type: 'object',
+          required: ['f'],
+          allOf: [{
+            type: 'object',
+            required: ['g'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+          anyOf: [{
+            type: 'object',
+            required: ['h'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+          oneOf: [{
+            type: 'object',
+            required: ['i'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+        }],
+        oneOf: [{
+          type: 'object',
+          required: ['j'],
+          allOf: [{
+            type: 'object',
+            required: ['k'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+          anyOf: [{
+            type: 'object',
+            required: ['l'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+          oneOf: [{
+            type: 'object',
+            required: ['m'],
+            allOf: null,
+            anyOf: null,
+            oneOf: null,
+          }],
+        }],
+      };
+
+      this.result = testUnit(defaultMocker, 'mergeCombinedSchemasForType', singleTypedSchema);
+    });
+
+    it('merges all nested combined schemas', function () {
+      expect(this.result).to.eql({
+        type: 'object',
+        required: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'],
       });
     });
   });
