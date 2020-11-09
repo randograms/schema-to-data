935bc87b9eee639110feb9aba3fc2c4fff5fdfa3
Rachid Lamouri <rachidlamouri@gmail.com>
2020-11-08 20:16:11 -0500

    wipperoni

--- tests/unit/helpers/setupCustomMocker.js
+++ tests/unit/helpers/setupCustomMocker.js
@@ -1,12 +1,20 @@
+const { expect } = require('chai');
 const _ = require('lodash');
 const { Mocker, defaultMocker } = require('../../../lib/mocker');
 
 module.exports.setupCustomMocker = (options) => {
   before(function () {
+    const {
+      onUnknownPattern,
+      ...defaults
+    } = options;
     this.mocker = new Mocker(options);
 
-    _.keys(options).forEach((option) => {
-      expect(this.mocker.DEFAULTS[option]).to.not.equal(defaultMocker.DEFAULTS[option]);
+    _.keys(defaults).forEach((defaultName) => {
+      expect(this.mocker.DEFAULTS[defaultName], defaultName).to.not.equal(defaultMocker.DEFAULTS[defaultName]);
     });
+
+    expect(this.mocker.OPTIONS.onUnknownPattern, 'onUnknownPattern')
+      .to.not.equal(defaultMocker.OPTIONS.onUnknownPattern);
   });
 };
